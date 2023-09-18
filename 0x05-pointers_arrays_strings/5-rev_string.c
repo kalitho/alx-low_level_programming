63a838ba6ec1e7afa11efd3d90c9300e7e279750
kalitho <kingsouvage955@gmail.com>
2023-09-18 05:29:24 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * rev_string - reverses a string
- * @s: input string
- * Return: string in reverse
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
 void rev_string(char *s)
 {
