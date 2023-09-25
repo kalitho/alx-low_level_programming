8df01c4ac5a5dd1a2e052ece2883eb6d66ddd7da
kalitho <kingsouvage955@gmail.com>
2023-09-25 12:43:15 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
-	int r = 0;
-	int i = n;
+	unsigned int i = 0;
 
-	for (; r < i; r++)
+	while (i < n)
 	{
-		dest[r] = src[r];
-		n--;
+		dest[i] = src[i];
+	        i++;
 	}
+
 	return (dest);
 }
